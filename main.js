662fa9b35d789bf031fc9a370b9611d4cd50781e
czajka.marcin@protonmail.com <czajka.marcin@protonmail.com>
2019-03-27 22:52:30 +0100

    Add basic user input
    -Added text form in index.html intended for user to enter his city or
    postal code.
    -Added event listener for said input which on 'submit' triggers
    getCity function.
    -Added getCity function which checks if user input contains special characters,
    combination of numbers and letters or is empty. It console logs output or error.
    If succesfull, function changes city variable to city entered by user or postal
    code and triggers fetchCurrent() and fetchForecast() for that city.

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const appId = '0ae6c1ab2f3771bcf82ab2f9738ba430';
 let units = 'metric'; // jeśli chcemy wyświetlać tez w Fahrenheitach
 let searchMethod = 'q';    // jeśli chcemy umozliwić wyszukiwanie po czymś innym niz nazwa miasta
 
+let cityInput = document.getElementById('cityInput');
+let city = '';
+
+cityInput.addEventListener('submit', getCity);
 
 function fetchCurrent(query) {
     // current weather
@@ -21,3 +25,36 @@ function fetchForecast(query) {
 
 fetchCurrent('Wrocław');
 fetchForecast('Wrocław');
+
+function getCity(e) {
+    e.preventDefault();
+
+    //list of special chars that we dont want in our string
+    const regex = /[!$%^&*()_+|~=`{}\[\]:\/;<>?,.@#]/;
+
+    city = e.target[0].value.toLowerCase().trim();
+
+    if (!city) {
+        console.log('FormField is empty');
+    } else if (city.match(regex) || city.match(/[0-9]/) && city.match(/[a-z]/)) {
+        console.log('Invalid input')
+        city = '';
+    } else if (city[0].match(/[0-9]/)) {
+        if (city.length === 5) {
+            city = `Postal: ${city.slice(0, 2)}-${city.slice(2, 5)}`;
+        } else if (city.length === 6 && city[2] === '-') {
+            city = 'Postal: ' + city;
+        } else {
+            console.log('Invalid Postal code');
+            city = '';
+        }
+        if (city) console.log(city);
+    } else {
+        console.log(city);
+    }
+
+    if(city){
+        fetchCurrent(city);
+        fetchForecast(city);
+    }
+}
